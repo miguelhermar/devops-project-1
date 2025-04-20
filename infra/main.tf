2288154df82464a2e84729e43032649a1e73f288
Miguel Ángel Hernández <miguelangel.hermar410@gmail.com>
2025-04-20 17:24:07 -0600

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,10 +1,3 @@
-/*module "s3" {
-  source      = "./s3"
-  bucket_name = var.bucket_name
-  name        = var.name
-  environment = var.bucket_name
-}*/
-
 module "networking" {
   source               = "./networking"
   vpc_cidr             = var.vpc_cidr
@@ -32,7 +25,7 @@ module "ec2" {
   sg_enable_ssh_https      = module.security_group.sg_ec2_sg_ssh_http_id
   ec2_sg_name_for_python_api     = module.security_group.sg_ec2_for_python_api
   enable_public_ip_address = true
-  user_data_install_apache = templatefile("./template/ec2_install_apache.sh", {})
+  user_data_install_apache = templatefile("./template/ec2_python_application.sh", {})
 }
 
 module "lb_target_group" {
@@ -57,9 +50,9 @@ module "alb" {
   lb_listner_port           = 5000
   lb_listner_protocol       = "HTTP"
   lb_listner_default_action = "forward"
-  lb_https_listner_port     = 443
-  lb_https_listner_protocol = "HTTPS"
-  dev_proj_1_acm_arn        = module.aws_ceritification_manager.dev_proj_1_acm_arn
+  # lb_https_listner_port     = 443
+  # lb_https_listner_protocol = "HTTPS"
+  # dev_proj_1_acm_arn        = module.aws_ceritification_manager.dev_proj_1_acm_arn
   lb_target_group_attachment_port = 5000
 }
 
@@ -70,11 +63,11 @@ module "hosted_zone" {
   aws_lb_zone_id  = module.alb.aws_lb_zone_id
 }
 
-module "aws_ceritification_manager" {
-  source         = "./certificate-manager"
-  domain_name    = var.domain_name
-  hosted_zone_id = module.hosted_zone.hosted_zone_id
-}
+# module "aws_ceritification_manager" {
+#   source         = "./certificate-manager"
+#   domain_name    = var.domain_name
+#   hosted_zone_id = module.hosted_zone.hosted_zone_id
+# }
 
 module "rds_db_instance" {
   source               = "./rds"
