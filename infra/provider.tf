70b5d874c706595087b3a08f13564695ed32e4a3
Miguel Ángel Hernández <miguelangel.hermar410@gmail.com>
2025-04-20 17:53:25 -0600

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,4 +1,4 @@
 provider "aws" {
-  region                   = "eu-central-1"
-  shared_credentials_files = ["/Users/rahulwagh/.aws/credentials"]
+  region  = "us-west-2"
+  profile = var.aws_profile
 }
