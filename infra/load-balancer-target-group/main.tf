56241ff37d95e5186744d0c3160f1b0021f88c2c
Miguel Ángel Hernández <miguelangel.hermar410@gmail.com>
2025-04-20 18:09:36 -0600

    Update main.tf

--- infra/load-balancer-target-group/main.tf
+++ infra/load-balancer-target-group/main.tf
@@ -1,31 +1,81 @@
-variable "lb_target_group_name" {}
-variable "lb_target_group_port" {}
-variable "lb_target_group_protocol" {}
-variable "vpc_id" {}
-variable "ec2_instance_id" {}
-
-output "dev_proj_1_lb_target_group_arn" {
-  value = aws_lb_target_group.dev_proj_1_lb_target_group.arn
+module "networking" {
+  source               = "./networking"
+  vpc_cidr             = var.vpc_cidr
+  vpc_name             = var.vpc_name
+  cidr_public_subnet   = var.cidr_public_subnet
+  us_availability_zone = var.us_availability_zone
+  cidr_private_subnet  = var.cidr_private_subnet
 }
 
-resource "aws_lb_target_group" "dev_proj_1_lb_target_group" {
-  name     = var.lb_target_group_name
-  port     = var.lb_target_group_port
-  protocol = var.lb_target_group_protocol
-  vpc_id   = var.vpc_id
-  health_check {
-    path = "/health"
-    port = 5000
-    healthy_threshold = 6
-    unhealthy_threshold = 2
-    timeout = 2
-    interval = 5
-    matcher = "200"  # has to be HTTP 200 or fails
-  }
+module "security_group" {
+  source                     = "./security-groups"
+  ec2_sg_name                = "SG for EC2 to enable SSH(22) and HTTP(80)"
+  vpc_id                     = module.networking.dev_proj_1_vpc_id
+  public_subnet_cidr_block   = tolist(module.networking.public_subnet_cidr_block)
+  ec2_sg_name_for_python_api = "SG for EC2 for enabling port 5000"
 }
 
-resource "aws_lb_target_group_attachment" "dev_proj_1_lb_target_group_attachment" {
-  target_group_arn = aws_lb_target_group.dev_proj_1_lb_target_group.arn
-  target_id        = var.ec2_instance_id
-  port             = 5000
+module "ec2" {
+  source                   = "./ec2"
+  ami_id                   = var.ec2_ami_id
+  instance_type            = "t2.micro"
+  tag_name                 = "Ubuntu Linux EC2"
+  public_key               = var.public_key
+  subnet_id                = tolist(module.networking.dev_proj_1_public_subnets)[0]
+  sg_enable_ssh_https      = module.security_group.sg_ec2_sg_ssh_http_id
+  ec2_sg_name_for_python_api     = module.security_group.sg_ec2_for_python_api
+  enable_public_ip_address = true
+  user_data_install_apache = templatefile("./template/ec2_python_application.sh", {})
+}
+
+module "lb_target_group" {
+  source                   = "./load-balancer-target-group"
+  lb_target_group_name     = "dev-proj-1-lb-target-group-us-west-2"
+  lb_target_group_port     = 5000
+  lb_target_group_protocol = "HTTP"
+  vpc_id                   = module.networking.dev_proj_1_vpc_id
+  ec2_instance_id          = module.ec2.dev_proj_1_ec2_instance_id
+}
+
+module "alb" {
+  source                    = "./load-balancer"
+  lb_name                   = "dev-proj-1-alb"
+  is_external               = false
+  lb_type                   = "application"
+  sg_enable_ssh_https       = module.security_group.sg_ec2_sg_ssh_http_id
+  subnet_ids                = tolist(module.networking.dev_proj_1_public_subnets)
+  tag_name                  = "dev-proj-1-alb"
+  lb_target_group_arn       = module.lb_target_group.dev_proj_1_lb_target_group_arn
+  ec2_instance_id           = module.ec2.dev_proj_1_ec2_instance_id
+  lb_listner_port           = 5000
+  lb_listner_protocol       = "HTTP"
+  lb_listner_default_action = "forward"
+  # lb_https_listner_port     = 443
+  # lb_https_listner_protocol = "HTTPS"
+  # dev_proj_1_acm_arn        = module.aws_ceritification_manager.dev_proj_1_acm_arn
+  lb_target_group_attachment_port = 5000
+}
+
+module "hosted_zone" {
+  source          = "./hosted-zone"
+  domain_name     = var.domain_name
+  aws_lb_dns_name = module.alb.aws_lb_dns_name
+  aws_lb_zone_id  = module.alb.aws_lb_zone_id
+}
+
+# module "aws_ceritification_manager" {
+#   source         = "./certificate-manager"
+#   domain_name    = var.domain_name
+#   hosted_zone_id = module.hosted_zone.hosted_zone_id
+# }
+
+module "rds_db_instance" {
+  source               = "./rds"
+  db_subnet_group_name = "dev_proj_1_rds_subnet_group"
+  subnet_groups        = tolist(module.networking.dev_proj_1_public_subnets)
+  rds_mysql_sg_id      = module.security_group.rds_mysql_sg_id
+  mysql_db_identifier  = "mydb"
+  mysql_username       = "dbuser"
+  mysql_password       = "dbpassword"
+  mysql_dbname         = "devprojdb"
 }
